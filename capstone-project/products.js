e224d839aaa88e5691e255b56f0d17afe74dd89d
Anel49 <andels4949@students.ecpi.edu>
2025-03-21 15:13:51 -0400

    added category and stock, added some css to keep the elements closer together

--- capstone-project/products.js
+++ capstone-project/products.js
@@ -3,7 +3,9 @@ const productArr = [
     {
         img: "default-img.jpg",
         name: "Product 1",
+        category: "Category",
         price: 9.99,
+        stock: 8,
         description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed 
                    facilisis hendrerit mauris volutpat mollis. Cras vestibulum 
                    lectus orci, a ullamcorper sem fermentum eget. Curabitur 
@@ -13,7 +15,9 @@ const productArr = [
     {
         img: "default-img.jpg",
         name: "Product 2",
+        category: "Category",
         price: 9.99,
+        stock: 8,
         description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed 
                    facilisis hendrerit mauris volutpat mollis. Cras vestibulum 
                    lectus orci, a ullamcorper sem fermentum eget. Curabitur 
@@ -23,7 +27,9 @@ const productArr = [
     {
         img: "default-img.jpg",
         name: "Product 3",
+        category: "Category",
         price: 9.99,
+        stock: 8,
         description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed 
                    facilisis hendrerit mauris volutpat mollis. Cras vestibulum 
                    lectus orci, a ullamcorper sem fermentum eget. Curabitur 
@@ -33,7 +39,9 @@ const productArr = [
     {
         img: "default-img.jpg",
         name: "Product 4",
+        category: "Category",
         price: 9.99,
+        stock: 8,
         description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed 
                    facilisis hendrerit mauris volutpat mollis. Cras vestibulum 
                    lectus orci, a ullamcorper sem fermentum eget. Curabitur 
@@ -43,7 +51,9 @@ const productArr = [
     {
         img: "default-img.jpg",
         name: "Product 5",
+        category: "Category",
         price: 9.99,
+        stock: 8,
         description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed 
                    facilisis hendrerit mauris volutpat mollis. Cras vestibulum 
                    lectus orci, a ullamcorper sem fermentum eget. Curabitur 
@@ -63,7 +73,9 @@ $(document).ready(function(){
                 <div>
                     <img src="${key.img}">
                     <h3>${key.name}</h3>
+                    <p>${key.category}</p>
                     <h4>$${key.price}</h4>
+                    <p>${key.stock} in Stock</p>
                     <p>${key.description}</p>
                 </div>
                 `);
