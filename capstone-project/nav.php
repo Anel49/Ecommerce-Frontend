1260cc6982cf678fee2da5fa7b383e4d77d16021
Anel49 <142068438+Anel49@users.noreply.github.com>
2025-03-25 16:54:01 -0400

    Messed up repo
    Lazily added files using drag and drop 😅

---
+++ capstone-project/nav.php
@@ -0,0 +1,11 @@
+<?php
+    <header>
+    <nav>
+        <ul>
+            <li><a class="logo" href="products.html">Pet Warehouse</a></li>
+            <li><a href="products.html">All Products</a></li>
+            <li><a href="">About</a></li>
+        </ul>
+    </nav>
+    </header>
+?>
